7eab62522c6c14217cb4af6b16b6265ff3e8d239
eztaskomaz <eztaskomaz@gmail.com>
2021-03-07 16:36:58 +0300

    implemented the details component

--- src/component/Details/Details.js
+++ src/component/Details/Details.js
@@ -3,12 +3,12 @@ import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
 import useStyles from './styles';
 
-const Details = () => {
+const Details = ({ title }) => {
     const classes = useStyles();
 
     return (
-        <Card className={classes.income}>
-            <CardHeader title="Income"/>
+        <Card className={title === 'Income' ? classes.income: classes.expense}>
+            <CardHeader title={title}/>
             <CardContent>
                 <Typography variant="h5">
                     $50
