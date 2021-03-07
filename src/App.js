f4eb9c1556b69053aff36137b5a3e06777611e0c
eztaskomaz <eztaskomaz@gmail.com>
2021-03-07 16:09:09 +0300

    implemented the details component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Grid } from '@material-ui/core';
+
+import Details from "./component/Details/Details";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div>
-            Expense Tracker App
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+            </Grid>
         </div>
     );
 }
